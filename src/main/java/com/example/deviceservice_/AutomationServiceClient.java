fca4e20aa3dd2eeaafe1170461941a918bce6c5a
G00405094 <G00405094@atu.ie>
2024-01-09 21:19:39 +0000

    Created DeviceController

--- src/main/java/com/example/deviceservice_/AutomationServiceClient.java
+++ src/main/java/com/example/deviceservice_/AutomationServiceClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "confirmation-service", url = "http://localhost:8080")
+@FeignClient(name = "automation-service", url = "http://localhost:8081")
 public interface AutomationServiceClient {
-    @PostMapping("/confirm")
+    @PostMapping("/automation")
     String someDetails(@RequestBody DeviceDetails deviceDetails);
 }
